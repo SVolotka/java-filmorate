3eb869ec4a4d7596a1cef19079b94d8be31e64e9
Sergei Volotka <s.volotka73@gmail.com>
2025-11-13 23:37:49 +0600

    Deleted @Size()

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +18,6 @@ public class User {
     @Email
     private String email;
     @NotBlank(message = "Логин не должен быть null или пуст")
-    @Size(min = 5, max = 10, message = "Длина логина должна быть от 5 до 10 символов")
     @Pattern(regexp = "\\S+", message = "Логин не должен содержать пробелы")
     private String login;
     private String name;
